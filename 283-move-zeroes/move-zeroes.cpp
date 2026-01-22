c1a564372b5bbbefbf5b6baf62442858bc9a758d
Amish Verma <143865265+theamishdev@users.noreply.github.com>
2026-01-22 10:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    int left=0;
-    for(int i=0;i<=nums.size()-1;i++){
-        if(nums[i]==0){
-            continue;
+        int index=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[index++]=nums[i];
+            }}
+        while(index<nums.size()){
+            nums[index++]=0;
         }
-        else{
-            swap(nums[i],nums[left]);
-            left++;
-        }
-    }
+            
 }};
